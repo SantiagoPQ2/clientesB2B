2a15ac796e8c14e52317f89276d711029333da5b
SantiagoPQ2 <santiago@ibarazi.com.ar>
2025-12-04 10:16:09 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 
 import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 import { AuthProvider, useAuth } from "./context/AuthContext";
 import { ProductModalProvider } from "./context/ProductModalContext";
@@ -22,7 +23,7 @@ import PedidosB2B from "./pages/b2b/Pedidos";
 // Extras
 import Settings from "./pages/Settings";
 import Login from "./pages/Login";
-import Info from "./pages/Info";   // ⭐ AGREGADO
+import Info from "./pages/Info"; // ⭐ AGREGADO
 
 // ChatBot
 import ChatBubble from "./components/ChatBubble";
@@ -48,7 +49,7 @@ function ProtectedApp() {
         <Route path="/b2b/catalogo" element={<CatalogoB2B />} />
         <Route path="/b2b/carrito" element={<CarritoB2B />} />
         <Route path="/b2b/pedidos" element={<PedidosB2B />} />
-        <Route path="/info" element={<Info />} /> {/* ⭐ NUEVA PAGE */}
+        <Route path="/info" element={<Info />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="*" element={<PromosB2B />} />
       </Routes>
@@ -69,12 +70,23 @@ function ProtectedApp() {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-br from-blue-50 to-indigo-100">
 
+      {/* NAVIGATION */}
       <Navigation />
 
-      <main className="flex-1 overflow-hidden">{allowedRoutes}</main>
+      {/* CONTENIDO PRINCIPAL */}
+      <main className="flex-1 overflow-hidden">
+        {allowedRoutes}
+      </main>
 
-      {hasUpdate && <UpdateBanner onReload={() => window.location.reload()} />}
+      {/* FOOTER */}
+      <Footer />
 
+      {/* UPDATE BANNER */}
+      {hasUpdate && (
+        <UpdateBanner onReload={() => window.location.reload()} />
+      )}
+
+      {/* CHATBOT */}
       {showChatBot && !openChat && (
         <ChatBubble onOpen={() => setOpenChat(true)} />
       )}
